11f3fd4bbcc52210e52fef9c4da6c2e86831844a
nguyentrong01 <s2.thedemons@gmail.com>
2016-03-08 20:11:25 +0700

    sédfsfs

---
+++ trongntps03136_assignment/assignment/frmTrangChinh.vb
@@ -0,0 +1,18 @@
+﻿Public Class frmTrangChinh
+
+    Private Sub ThêmSảnPhẩmToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ThêmSảnPhẩmToolStripMenuItem.Click
+        frmThemSP.Show()
+    End Sub
+
+    Private Sub XemKhácHàngToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles XemKhácHàngToolStripMenuItem.Click
+        frmKhachHang.Show()
+    End Sub
+
+    Private Sub ChỉnhSữaKHToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChỉnhSữaKHToolStripMenuItem.Click
+        frmThemKH.Show()
+    End Sub
+
+    Private Sub XemSảnPhẩmToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles XemSảnPhẩmToolStripMenuItem.Click
+        frmXemSP.Show()
+    End Sub
+End Class
